a9c15cb5a1e973777768ce915731bc8f6b7b5d6a
Siyan <siyanliuads@gmail.com>
2025-10-05 17:04:04 -0400

    parser done, added indexer

--- HW2/scratchpad.py
+++ HW2/scratchpad.py
@@ -1,8 +1,10 @@
-from ftfy import fix_text
+# from ftfy import fix_text
 
-with open("data/marco_tiny.tsv", "r", encoding="utf8") as f:
-    for i, line in enumerate(f):
-        text = fix_text(line)
-        print(text)
-        if i >= 10:
-            break
+# with open("data/marco_tiny.tsv", "r", encoding="utf8") as f:
+#     for i, line in enumerate(f):
+#         text = fix_text(line)
+#         print(text)
+#         if i >= 10:
+#             break
+
+print('1946'.islower())
